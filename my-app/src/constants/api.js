f9a08a10ef7068d76dacda4fd29a4cffaa1e90de
Nguyễn Trường <nguyentruongpro19@gmail.com>
2025-05-14 23:02:04 +0700

    Merge myuh to testMerge

--- my-app/src/constants/api.js
+++ my-app/src/constants/api.js
@@ -1,5 +1,5 @@
 export const URL_BACKEND = import.meta.env.VITE_BACKEND_URL
-
+export const URL_FRONTEND = import.meta.env.VITE_FRONTEND_URL
 export const ROUTES = {
   HOME: '/',
   LOGIN: '/login',
@@ -14,8 +14,8 @@ export const ROUTES = {
   DASHBOARD: '/dashboard',
   FORGOT_PASSWORD: '/forgot-password',
   USERS: '/users/:id', // Đổi sang dạng /users/:id
-  USER_BY_ID: '/users/:id', 
-  USER_BY_ID_PUBLIC: '/users/:id/public', 
+  USER_BY_ID: '/users/:id',
+  USER_BY_ID_PUBLIC: '/users/:id/public',
   COMMENT: '/comment',
   REPORT: '/report',
   FAVOURITE: '/favourite',
