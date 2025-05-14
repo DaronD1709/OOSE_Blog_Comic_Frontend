95005464d25c31f91b0d86ca3847dd3adcc68bd1
DaronD1709 <110324724+DaronD1709@users.noreply.github.com>
2025-05-15 01:28:54 +0700

    Change background image

--- my-app/src/pages/LoginPage.jsx
+++ my-app/src/pages/LoginPage.jsx
@@ -10,19 +10,19 @@ import {
 } from "antd";
 import { Link, useNavigate } from "react-router-dom";
 import { loginAPI } from "../services/authService.js";
-import { ROUTES, URL_BACKEND } from '../constants/api.js'
+import { ROUTES, URL_BACKEND } from "../constants/api.js";
 
 const LoginPage = () => {
-  const [form] = Form.useForm()
-  const [verifyEmail, setVerifyEmail] = useState(false)
-  const navigate = useNavigate()
+  const [form] = Form.useForm();
+  const [verifyEmail, setVerifyEmail] = useState(false);
+  const navigate = useNavigate();
   const handleGoogleLogin = () => {
-    window.location.href = URL_BACKEND + '/oauth2/authorization/google'
-  }
+    window.location.href = URL_BACKEND + "/oauth2/authorization/google";
+  };
   const goToVerifyEmail = () => {
-    navigate(ROUTES.REGISTER, { state: { step: 2 } })
-  }
-  const [api, contextHolder] = notification.useNotification()
+    navigate(ROUTES.REGISTER, { state: { step: 2 } });
+  };
+  const [api, contextHolder] = notification.useNotification();
   const openNotification = () => {
     api.open({
       message: "Tài khoản chưa được xác thực Email",
@@ -49,14 +49,14 @@ const LoginPage = () => {
       const response = await loginAPI(identifier, password);
       console.log(">>> Check login response ", response);
       if (response.accessToken === null) {
-        openNotification()
-        localStorage.setItem('userId', response.data.userId)
-        setVerifyEmail(true)
+        openNotification();
+        localStorage.setItem("userId", response.data.userId);
+        setVerifyEmail(true);
       } else {
-        const accessToken = response.accessToken
-        localStorage.setItem('access_token', accessToken)
-        message.success('Đang nhập thành công ')
-        navigate('/')
+        const accessToken = response.accessToken;
+        localStorage.setItem("access_token", accessToken);
+        message.success("Đang nhập thành công ");
+        navigate("/");
       }
     } catch (error) {
       message.error("Đăng nhập thất bại");
@@ -71,7 +71,9 @@ const LoginPage = () => {
           {/* Left Section: Login Form */}
           <div className="w-1/2 p-10">
             <div className="flex items-center mb-8">
-              <div className="text-blue-600 text-2xl font-bold">Comic</div>
+              <div style={{ color: "#8DAFB1" }} className="text-2xl font-bold">
+                Comic
+              </div>
             </div>
             <h1 className="text-3xl font-bold mb-2">Welcome Back</h1>
             <p className="text-gray-500 mb-6">
@@ -81,30 +83,34 @@ const LoginPage = () => {
               className="space-y-4 mt-4"
               form={form}
               onFinish={(values) => onFinish(values)}
-              layout={'vertical'}
+              layout={"vertical"}
             >
               <Form.Item name="username" label="Email / Username">
-                <Input/>
+                <Input />
               </Form.Item>
 
               <Form.Item name="password" label="Password">
-                <Input.Password/>
+                <Input.Password />
               </Form.Item>
               <div className="flex items-center justify-between">
                 <label className="flex items-center">
-                  <Checkbox type="checkbox" className="mr-2"/>
+                  <Checkbox type="checkbox" className="mr-2" />
                   <span className="text-sm ml-2 text-gray-600">
                     Remember Me
                   </span>
                 </label>
-                <a href="#" className="text-sm text-blue-600 hover:underline">
+                <a
+                  href="#"
+                  style={{ color: "#8DAFB1" }}
+                  className="text-sm text-blue-600 hover:underline"
+                >
                   Forget Your Password?
                 </a>
               </div>
               <Button
-                className="w-full bg-blue-600  text-amber-50 py-2 rounded-md hover:bg-blue-700 transition"
+                className="w-full text-amber-50 py-2 rounded-md hover:bg-blue-700 transition"
                 onClick={() => {
-                  form.submit()
+                  form.submit();
                 }}
               >
                 Log In
@@ -112,7 +118,7 @@ const LoginPage = () => {
             </Form>
 
             <div className="mt-4 text-center ">
-              <Divider plain={'false'}>Or Log In With</Divider>
+              <Divider plain={"false"}>Or Log In With</Divider>
               <div className="flex justify-center gap-3 mb-4  mt-2">
                 <Button
                   onClick={handleGoogleLogin}
@@ -127,8 +133,12 @@ const LoginPage = () => {
                 </Button>
               </div>
               <p className="mt-5 text-sm text-gray-600 ">
-                Don’t Have an Account?{' '}
-                <Link className="text-blue-600 hover:underline" to={ROUTES.REGISTER}>
+                Don’t Have an Account?{" "}
+                <Link
+                  style={{ color: "#8DAFB1" }}
+                  className="text-blue-600 hover:underline"
+                  to={ROUTES.REGISTER}
+                >
                   Register Now.
                 </Link>
               </p>
@@ -141,49 +151,17 @@ const LoginPage = () => {
             </div>
           </div>
           {/* Right Section: Promotional Content */}
-          <div className="w-1/2 bg-blue-600 p-10 text-white flex flex-col justify-center">
-            <h2 className="text-3xl font-bold mb-4">
-              Effortlessly manage your team and operations.
-            </h2>
-            <p className="mb-6">
-              Log in to access the CRM dashboard and manage your team.
-            </p>
-            <div className="relative">
-              <div className="bg-white p-4 rounded-lg shadow-lg">
-                <div className="grid grid-cols-2 gap-4">
-                  <div className="bg-gray-100 p-3 rounded-md">
-                    <p className="text-sm text-gray-600">Total Sales</p>
-                    <p className="text-xl font-bold">$189,374</p>
-                  </div>
-                  <div className="bg-gray-100 p-3 rounded-md">
-                    <p className="text-sm text-gray-600">Chat Performance</p>
-                    <p className="text-xl font-bold">00:01:30</p>
-                  </div>
-                  <div className="bg-gray-100 p-3 rounded-md col-span-2">
-                    <p className="text-sm text-gray-600">Sales Overview</p>
-                    <div className="h-16 bg-gray-300 rounded mt-2"></div>
-                  </div>
-                </div>
-              </div>
-              <div className="absolute -bottom-4 -right-4 bg-white p-4 rounded-lg shadow-lg">
-                <div className="bg-gray-100 p-3 rounded-md">
-                  <p className="text-sm text-gray-600">Total Profit</p>
-                  <p className="text-xl font-bold">$25,684</p>
-                </div>
-              </div>
-              <div className="absolute top-0 right-0 bg-white p-4 rounded-lg shadow-lg">
-                <div className="bg-gray-100 p-3 rounded-md">
-                  <p className="text-sm text-gray-600">Sales Categories</p>
-                  <div className="h-16 bg-purple-300 rounded mt-2"></div>
-                  <p className="text-sm text-gray-600 mt-2">6,248 Units</p>
-                </div>
-              </div>
-            </div>
+          <div className="w-1/2 bg-blue-600 text-white flex flex-col justify-center">
+            <img
+              src="https://i.pinimg.com/736x/86/f1/53/86f15337d7e8aa9a77ab5e0d52d2c2a4.jpg"
+              className="h-full w-full object-cover"
+              alt="background-login"
+            />
           </div>
         </div>
       </div>
     </>
-  )
+  );
 };
 
 export default LoginPage;
