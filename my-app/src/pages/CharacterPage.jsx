ba8e299906fe24bd34347e6f2f2dc1473fe8bb43
DaronD1709 <110324724+DaronD1709@users.noreply.github.com>
2025-05-16 22:34:10 +0700

    Change height

--- my-app/src/pages/CharacterPage.jsx
+++ my-app/src/pages/CharacterPage.jsx
@@ -1,45 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import AppPagination from '../components/AppPagination'
-import VerticalCard from '../components/Card/VerticalCard'
-import { getBlogCharacterPaginationAPI, getBlogComicPaginationAPI } from '../services/blogService.js'
-import { PAGINATION } from '../constants/api.js'
-import { message } from 'antd'
-import AppSidebar from '../components/Sidebar/AppSidebar.jsx'
-import { validate } from '../utils/validate.js'
+import React, { useEffect, useState } from "react";
+import AppPagination from "../components/AppPagination";
+import VerticalCard from "../components/Card/VerticalCard";
+import {
+  getBlogCharacterPaginationAPI,
+  getBlogComicPaginationAPI,
+} from "../services/blogService.js";
+import { PAGINATION } from "../constants/api.js";
+import { message } from "antd";
+import AppSidebar from "../components/Sidebar/AppSidebar.jsx";
+import { validate } from "../utils/validate.js";
 
 const ReviewPageMenu = [
   {
-    label: 'Thể loại truyện',
+    label: "Thể loại truyện",
     children: [
-      { label: 'Action', to: '/genre/action' },
-      { label: 'Adventure', to: '/genre/adventure' },
-      { label: 'Drama', to: '/genre/drama' },
-      { label: 'Fantasy', to: '/genre/fantasy' },
-      { label: 'Romance', to: '/genre/romance' },
+      { label: "Action", to: "/genre/action" },
+      { label: "Adventure", to: "/genre/adventure" },
+      { label: "Drama", to: "/genre/drama" },
+      { label: "Fantasy", to: "/genre/fantasy" },
+      { label: "Romance", to: "/genre/romance" },
     ],
   },
-  { label: 'Tất cả truyện', to: '/all-comics' },
-]
+  { label: "Tất cả truyện", to: "/all-comics" },
+];
 
 const CharacterPage = () => {
-  const [currentPage, setCurrentPage] = useState(1)
-  const [selectedGenre, setSelectedGenre] = useState(null)
-  const [blogs, setBlogs] = useState([])
-  const [meta, setMeta] = useState(null)
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedGenre, setSelectedGenre] = useState(null);
+  const [blogs, setBlogs] = useState([]);
+  const [meta, setMeta] = useState(null);
   useEffect(() => {
-    getBlogs()
-  }, [currentPage])
+    getBlogs();
+  }, [currentPage]);
   const getBlogs = async () => {
     try {
-      const res = await getBlogCharacterPaginationAPI(currentPage - 1, PAGINATION.SIZE)
-      setBlogs(res.result)
-      setMeta(res.meta)
+      const res = await getBlogCharacterPaginationAPI(
+        currentPage - 1,
+        PAGINATION.SIZE
+      );
+      setBlogs(res.result);
+      setMeta(res.meta);
     } catch (err) {
-      message.error(err.data)
+      message.error(err.data);
     }
-  }
+  };
   return (
-    <div className="flex h-full ">
+    <div className="flex h-[1130px] ">
       <div>
         <AppSidebar
           // Truyền menu và hàm xử lý sự kiện chọn thể loại
@@ -51,28 +57,24 @@ const CharacterPage = () => {
       <div className="flex flex-col flex-1 pt-10 px-3 ">
         <div className="flex flex-wrap gap-5 h-[930px] justify-start overflow-y-scroll">
           {validate(blogs) ? (
-            blogs.map((comic) => (
-              <VerticalCard key={comic.id} {...comic} />
-            ))
+            blogs.map((comic) => <VerticalCard key={comic.id} {...comic} />)
           ) : (
             <p>Không có truyện nào cho thể loại này.</p>
           )}
         </div>
         <div className="flex justify-center mt-5 mb-13 pb-5">
-          {
-            validate(meta) &&
+          {validate(meta) && (
             <AppPagination
               current={currentPage}
               total={meta.total}
               pageSize={meta.pageSize}
               onChange={setCurrentPage}
             />
-          }
-
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CharacterPage
+export default CharacterPage;
